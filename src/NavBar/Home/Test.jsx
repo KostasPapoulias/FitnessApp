5e8416d0eaf15c8a4f400a3b3eb40e854326ba71
Konstantinos Papoulias <kostaspapoulias@gmail.com>
2024-05-23 14:36:03 +0200

    Refactor SVG image paths and add new images for NavBar/Home component

--- src/NavBar/Home/Test.jsx
+++ src/NavBar/Home/Test.jsx
@@ -1,12 +1,16 @@
 
 import { SvgLoader, SvgProxy } from "react-svgmt";
-import human from '../../../pictures/good_front_side.svg';
+import human1 from '../../../pictures/good_front_side.svg';
+import human2 from '../../../pictures/good_back_side.svg';
+import flip from '../../../pictures/flip.png';
+
 import { useEffect } from "react";
 
 import { useState } from "react";
 export default function Test({WoMuscles}){
 
     const [muscles, setMuscles] = useState([]);
+    const uniqueMuscles = [...new Set(muscles)];
 
     useEffect(() => {
       console.log("workedMuscles: ", WoMuscles);
@@ -16,21 +20,60 @@ export default function Test({WoMuscles}){
       }
     }, [WoMuscles]);
 
+
+    const [isHuman1, setIsHuman1] = useState(true);
+
+    const handleSwitchImage = () => {
+        setIsHuman1(!isHuman1);
+    };
+    
+    const [isSideA, setSideA] = useState(true);
+
+    const handleSwitchSide = () => {
+        setSideA(!isSideA);
+    };
+
+    const handleClick = () => {
+        handleSwitchImage();
+        handleSwitchSide();
+    };
+
     return(
         <div className='HumanContainer'>
-            <SvgLoader path={human}>
-        {/* Important! this proxy will reset the color to black,
-          otherwise old elements would still be shown in red
-          because this library doesn't store previous states */}
-        <SvgProxy selector={"path"} fill="white" />
-        {muscles.map(code => (
-          <SvgProxy
-            key={code}
-            selector={"#" + code + ",#" + code + " path"}
-            fill="red"
-          />
-        ))}
-      </SvgLoader>
+
+                {isHuman1 ? (
+                     <SvgLoader path={human1}>
+                     {/* Important! this proxy will reset the color to black,
+                       otherwise old elements would still be shown in red
+                       because this library doesn't store previous states */}
+                     <SvgProxy selector={"path"} fill="white" />
+                     {uniqueMuscles.map(code => (
+                       <SvgProxy
+                         key={code}
+                         selector={"#" + code + ",#" + code + " path"}
+                         fill="red"
+                       />
+                     ))}
+                   </SvgLoader>
+                     
+                ) : (
+                  <SvgLoader path={human2}>
+                  {/* Important! this proxy will reset the color to black,
+                    otherwise old elements would still be shown in red
+                    because this library doesn't store previous states */}
+                  <SvgProxy selector={"path"} fill="white" />
+                  {uniqueMuscles.map(code => (
+                    <SvgProxy
+                      key={code}
+                      selector={"#" + code + ",#" + code + " path"}
+                      fill="red"
+                    />
+                  ))}
+                </SvgLoader>
+                )}
+                <button className='switchSide' onClick={handleClick}><img src={flip} alt="flip" style={{width: '50px'}}/></button>
+            
+          
         </div>
     );
 }
