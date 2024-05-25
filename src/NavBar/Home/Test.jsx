16a578e6fc312821223e5d09e666977e9c62b872
Konstantinos Papoulias <kostaspapoulias@gmail.com>
2024-05-25 14:37:33 +0200

    Refactor SVG image paths and add new images for NavBar/Home component

--- src/NavBar/Home/Test.jsx
+++ src/NavBar/Home/Test.jsx
@@ -3,6 +3,7 @@ import { SvgLoader, SvgProxy } from "react-svgmt";
 import human1 from '../../../pictures/good_front_side.svg';
 import human2 from '../../../pictures/good_back_side.svg';
 import flip from '../../../pictures/flip.png';
+import { useSelector } from "react-redux";
 
 import { useEffect } from "react";
 
@@ -38,6 +39,8 @@ export default function Test({WoMuscles}){
         handleSwitchSide();
     };
 
+    const recovery = useSelector(state => Array.isArray(state.categories.recovery) ? state.categories.recovery : []); 
+    console.log(recovery);
     return(
         <div className='HumanContainer'>
 
@@ -47,13 +50,40 @@ export default function Test({WoMuscles}){
                        otherwise old elements would still be shown in red
                        because this library doesn't store previous states */}
                      <SvgProxy selector={"path"} fill="white" />
-                     {uniqueMuscles.map(code => (
+                     {recovery.map((element,index )=> 
+                      element.count == 0 ? (
+                        <SvgProxy
+                          key={element.name + index}
+                          selector={"#" + element.name + ",#" + element.name + " path"}
+                          fill="white"
+                        />
+                      ) : element.count == 1 ? (
+                        <SvgProxy
+                          key={element.name + index}
+                          selector={"#" + element.name + ",#" + element.name + " path"}
+                          fill="cyan"
+                        />
+                      ) : element.count == 2 ? (
+                        <SvgProxy
+                          key={element.name + index}
+                          selector={"#" + element.name + ",#" + element.name + " path"}
+                          fill="pink"
+                        />
+                      ) : element.count == 3 ? (
+                        <SvgProxy
+                          key={element.name + index}
+                          selector={"#" + element.name + ",#" + element.name + " path"}
+                          fill="red"
+                        />
+                      ) : null
+                    )}
+                      {/* {recovery.map(code => (
                        <SvgProxy
                          key={code}
                          selector={"#" + code + ",#" + code + " path"}
                          fill="red"
                        />
-                     ))}
+                     ))} */}
                    </SvgLoader>
                      
                 ) : (
@@ -61,14 +91,42 @@ export default function Test({WoMuscles}){
                   {/* Important! this proxy will reset the color to black,
                     otherwise old elements would still be shown in red
                     because this library doesn't store previous states */}
-                  <SvgProxy selector={"path"} fill="white" />
-                  {uniqueMuscles.map(code => (
+                  {/* <SvgProxy selector={"path"} fill="white" />
+                  {recovery.map(code => (
                     <SvgProxy
                       key={code}
                       selector={"#" + code + ",#" + code + " path"}
                       fill="red"
                     />
-                  ))}
+                  ))} */}
+                   <SvgProxy selector={"path"} fill="white" />
+                    {recovery.map((element,index )=> 
+                      element.count == 0 ? (
+                        <SvgProxy
+                          key={element.name + index}
+                          selector={"#" + element.name + ",#" + element.name + " path"}
+                          fill="white"
+                        />
+                      ) : element.count == 1 ? (
+                        <SvgProxy
+                          key={element.name + index}
+                          selector={"#" + element.name + ",#" + element.name + " path"}
+                          fill="cyan"
+                        />
+                      ) : element.count == 2 ? (
+                        <SvgProxy
+                          key={element.name + index}
+                          selector={"#" + element.name + ",#" + element.name + " path"}
+                          fill="pink"
+                        />
+                      ) : element.count == 3 ? (
+                        <SvgProxy
+                          key={element.name + index}
+                          selector={"#" + element.name + ",#" + element.name + " path"}
+                          fill="red"
+                        />
+                      ) : null
+                    )}
                 </SvgLoader>
                 )}
                 <button className='switchSide' onClick={handleClick}><img src={flip} alt="flip" style={{width: '50px'}}/></button>
