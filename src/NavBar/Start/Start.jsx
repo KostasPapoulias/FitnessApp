6ed7b062ac5701e2ac127edcecc199b5ee02e980
Konstantinos Papoulias <kostaspapoulias@gmail.com>
2024-05-25 14:37:13 +0200

    update category count

--- src/NavBar/Start/Start.jsx
+++ src/NavBar/Start/Start.jsx
@@ -1,3 +1,4 @@
+import { Reviews } from '@mui/icons-material';
 import React, {useState} from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
@@ -6,12 +7,27 @@ import { useDispatch } from 'react-redux';
 export default function Start({workedMuscles, worked}){
     const dispatch = useDispatch();
     const list = useSelector(state => state.exercises.list);
+    const recovery = useSelector(state => state.categories.recovery);
 
     const handleBegin = () => {
         let groups = Array.isArray(list) ? list.map(item => item.category) : [];
-
+        console.log(groups);
         workedMuscles(groups);
         worked(true);
+
+
+        recovery.forEach(recovery => {
+            // dispatch({ type: 'INCREMENT_CATEGORY_COUNT', payload: group });
+            const match = groups.some(item => item === recovery.name);
+            match ? 
+                dispatch({ type: 'INCREMENT_CATEGORY_COUNT', payload: recovery.name })
+            :
+                dispatch({ type: 'DECREMENT_CATEGORY_COUNT', payload: recovery.name })
+        });
+
+        // dispatch({ type: 'INCREMENT_CATEGORY_COUNT', payload: 'Triceps' });
+
+
         dispatch({ type: 'CLEAR_LIST' });
         dispatch({type: 'CLEAR_CATEGORY'});
 
